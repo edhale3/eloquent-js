2531f201ad7d1da21c8c7e835ac80b03a6c03bf5
Edward Hale <edhale33@gmail.com>
2019-03-27 12:41:12 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express"),
+    app = express();
+
+app.get("/", function(req, res){
+    res.send("hello world")
+});
+
+function range (first, second, step) {
+    var arr = []
+    for(let i = first; i <= second; i+=step){
+        arr.push(i)
+    }
+    return arr;
+}
+function sum(range){
+    var total=0;
+    range.forEach(element => {
+        total=element+total;
+    });
+    return total;
+}
+console.log(sum(range(1,10,2)));
+
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000! hi')
+});
