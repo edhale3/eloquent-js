0f7afa9e0e5e3b60a48747ee610744ca9ce87ec3
Edward Hale <edhale33@gmail.com>
2019-03-27 12:58:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ app.get("/", function(req, res){
     res.send("hello world")
 });
 
+//program to take the range of two numbers and then output the sum of that range. 
+//step declares the increments by which it adds to the sum
 function range (first, second, step) {
     var arr = []
-    for(let i = first; i <= second; i+=step){
+    for(let i = first; i >= second; i+=step){
         arr.push(i)
     }
     return arr; 
@@ -19,7 +21,7 @@ function sum(range){
     });
     return total;
 }
-console.log(sum(range(1,10,2)));
+console.log(sum(range(10,1,-1)));
 
 app.listen(8000, () => {
     console.log('Example app listening on port 8000! hi')
