b4afd74597e9eb68c6316d657c4f6c76ba39aa01
Edward Hale <edhale33@gmail.com>
2019-03-27 23:15:46 -0700

    sample problems

--- app.js
+++ app.js
@@ -7,22 +7,81 @@ app.get("/", function(req, res){
 
 //program to take the range of two numbers and then output the sum of that range. 
 //step declares the increments by which it adds to the sum
-function range (first, second, step) {
-    var arr = []
-    for(let i = first; i >= second; i+=step){
-        arr.push(i)
-    }
-    return arr; 
-}
-function sum(range){
-    var total=0;
-    range.forEach(element => {
-        total=element+total;
-    });
-    return total;
-}
-console.log(sum(range(10,1,-1)));
+// function range (first, second, step) {
+//     var arr = []
+//     for(let i = first; i >= second; i+=step){
+//         arr.push(i)
+//     }
+//     return arr; 
+// }
+// function sum(range){
+//     var total=0;
+//     range.forEach(element => {
+//         total=element+total;
+//     });
+//     return total;
+// }
+// console.log(sum(range(10,1,-1)));
+
+//this program takes an array and reverses it
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let reverse =[];
+// function reverseArrayInPlace(arr){
+//     arr.forEach(element => {
+//        reverse.unshift(element); 
+//     });
+//     return reverse;
+// };
+
+// console.log(reverseArrayInPlace(arr));
+
+//convert an array to a list, the same list back to an array, use the helper function prepend to create a new list that adds the element to the front of the input list
+//and nth function which takes a list and a number and returns the element at the given position in the list
+    // function arrayToList(arr){
+    //     let ans = null;
+    //     for (let i = arr.length-1; i >= 0; i--){
+    //         ans = {
+    //             value:arr[i], 
+    //             rest: ans
+    //         }
+    //     };
+    //     return ans;
+    // }
+    // function listToArray(list) {
+    //     let array =[];
+    //     for(let node = list; node; node=node.rest){
+    //         array.push(node.value);
+    //     }
+    //     return array;
+    // }
+
+    // function prepend(value, list){
+    //     return {value, rest:list}
+    // }
+
+    // function nth(list, n){
+    //     if(!list) return undefined;
+    //     else if(n == 0) return list.value;
+    //     else return nth(list.rest, n-1)
+    // }
+
+    // console.log(arrayToList([10,20]));
+    // console.log(listToArray(arrayToList([10,20,30])));
+    // console.log(prepend(10, prepend(20, null)));
+    // console.log(nth(arrayToList([10,20,30]), 1));
+
+    //high order functions
+// function noisy (f) {
+//     return(...args) => {
+//         console.log("calling with", args);
+//         let result = f(...args);
+//         console.log("called with", args, ", returned", result);
+//         return result;
+//     };
+// }
+// noisy(Math.min)(4,3,2,1);
 
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000! hi')
+    console.log('Example app listening on port 8000!')
 });
